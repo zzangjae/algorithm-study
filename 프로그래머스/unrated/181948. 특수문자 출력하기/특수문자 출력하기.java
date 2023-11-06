2503fe9c4f582362dc148f73588500b7fb42b708
YOO ChangJae <dondegi5@gmail.com>
2023-11-06 13:50:51 +0900

    [level unrated] Title: 특수문자 출력하기, Time: 116.30 ms, Memory: 62.9 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181948. 특수문자 출력하기/특수문자 출력하기.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.io.*;
+
+public class Solution {
+    public static void main(String[] args) throw Exception {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        bw.write('!');
+        bw.write('@');
+        bw.write('#');
+        bw.write('$');
+        bw.write('%');
+        bw.write('^');
+        bw.write('&');
+        bw.write('*');
+        bw.write('(');
+        bw.write('\');
+        bw.write('%');
+        
+    }
+}
