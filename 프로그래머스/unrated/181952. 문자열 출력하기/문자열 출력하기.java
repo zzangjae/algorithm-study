6d9f4873f25a94dc569d816125b69eac3500204f
YOO ChangJae <dondegi5@gmail.com>
2023-11-06 10:47:54 +0900

    [level unrated] Title: 문자열 출력하기, Time: 125.46 ms, Memory: 62.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181952. 문자열 출력하기/문자열 출력하기.java
@@ -0,0 +1,11 @@
+import java.util.*;
+import java.io.*;
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        bw.write(br.readLine());
+        bw.flush();
+    }
+}
