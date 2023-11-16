a1b472ac42077214f4c2a100c262069187976d0b
YOO ChangJae <dondegi5@gmail.com>
2023-11-16 12:21:46 +0900

    [level 1] Title: 추억 점수, Time: 0.05 ms, Memory: 79.5 MB -BaekjoonHub

--- 프로그래머스/1/176963. 추억 점수/추억 점수.java
+++ 프로그래머스/1/176963. 추억 점수/추억 점수.java
@@ -1,27 +1,26 @@
 import java.util.*;
-
 class Solution {
     public int[] solution(String[] name, int[] yearning, String[][] photo) {
-        
-        int n = photo.length;
-        int[] answer = new int[n];
-        
-        Map<String, Integer> map = new HashMap<>();
-        
-        for (int i=0; i<name.length; i++) {
-            map.put(name[i], yearning[i]);
+        int[] answer = new int[photo.length];
+        HashMap<String,Integer> map = new LinkedHashMap<>();
+        for(int i=0; i< name.length; i++){
+            map.put(name[i],yearning[i]);
         }
-        
-        for (int i=0; i<n; i++) {
-            int sum = 0;
-            
-            for (String str: photo[i]) {
-                if (map.containsKey(str)) sum += map.get(str);
-            }        
-            
-            answer[i] = sum;
+
+        for(int i =0; i< photo.length;i++){
+            String[] persons = photo[i];
+            int score = 0;
+            for(int j=0; j<persons.length; j++){
+                String person = persons[j];
+                if(map.containsKey(person)){
+                    score+=map.get(person);
+                }
+            }
+            answer[i]=score;
         }
-        
+
         return answer;
+
+
     }
 }
