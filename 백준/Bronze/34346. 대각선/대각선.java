ddd660cf7e123142eb8f51b6a33b934280143374
유창재 <dondegi5@gmail.com>
2025-10-12 22:38:45 +0900

    [Bronze IV] Title: 대각선, Time: 64 ms, Memory: 11492 KB -BaekjoonHub

--- 백준/Bronze/34346. 대각선/대각선.java
+++ 백준/Bronze/34346. 대각선/대각선.java
@@ -3,14 +3,8 @@ import java.io.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         int n = Integer.parseInt(br.readLine());
-
-        StringBuilder sb = new StringBuilder();
-        if (n % 2 == 1) sb.append(1);
-        else sb.append(2);
-
-        System.out.println(sb);
+        System.out.println(n % 2 == 0 ? 2 : 1);
         br.close();
     }
 }
